096d70ca53cef12b36c0327b13dbe09b88f4d739
kasztelmacko <152562189+kasztelmacko@users.noreply.github.com>
2026-07-30 18:29:26 +0200

    added install dependencies action

--- src/proof/actions/install_dependencies.py
+++ src/proof/actions/install_dependencies.py
@@ -1,9 +1,38 @@
 from proof.actions.context import AnalysisContext
+from proof.config import REQUIRED_DEPENDENCIES
+import subprocess
 
 class InstallDependencies():
     def __init__(self, context: AnalysisContext):
         self.context = context
 
     def create(self) -> None:
-        pass
-    
+        pkg_manager = self.context.pkg_manager
+        analysis_root = self.context.analysis_root
+
+        if pkg_manager == "poetry":
+            subprocess.run(
+                ["poetry", "add", *REQUIRED_DEPENDENCIES],
+                cwd=analysis_root,
+                check=True,
+            )
+        elif pkg_manager == "uv":
+            subprocess.run(
+                ["uv", "pip", "install", *REQUIRED_DEPENDENCIES],
+                cwd=analysis_root,
+                check=True,
+            )
+        elif pkg_manager == "pip":
+            subprocess.run(
+                ["pip", "install", *REQUIRED_DEPENDENCIES],
+                cwd=analysis_root,
+                check=True,
+            )
+        elif pkg_manager == "conda":
+            subprocess.run(
+                ["conda", "install", *REQUIRED_DEPENDENCIES],
+                cwd=analysis_root,
+                check=True,
+            )
+        else:
+            raise ValueError(f"Invalid package manager: {pkg_manager}")
--- src/proof/config.py
+++ src/proof/config.py
@@ -1,14 +1,16 @@
 from typing import Literal
 
+PkgManager = Literal["poetry", "uv", "pip", "conda"]
+ModelsCLI = Literal["claude", "cursor", "copilot"]
+
 PROOF_CONFIG_FILE_NAME = "proof.toml"
 DEFAULT_NOTEBOOK_FILE_NAME = "notebook"
 
-PkgManager = Literal["poetry", "uv", "pip", "conda"]
+
+REQUIRED_DEPENDENCIES = ("marimo>=0.23.15",)
 PACKAGE_MANAGER_INDICATORS = {
     ("poetry.lock",) : "poetry",
     ("uv.lock",): "uv",
     ("environment.yml", "environment.yaml", "conda-lock.yml"): "conda",
     ("requirements.txt", "requirements-dev.txt", "setup.py", "setup.cfg"): "pip"
 } 
-
-ModelsCLI = Literal["claude", "cursor", "copilot"]
